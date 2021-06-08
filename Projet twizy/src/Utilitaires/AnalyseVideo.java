19953054040ecfcb13ccf4fbd8661806e2dd6f7f
Yuuuugo <69073794+Yuuuugo@users.noreply.github.com>
2021-06-08 17:31:24 +0200

    Fin Video

--- Projet twizy/src/Utilitaires/AnalyseVideo.java
+++ Projet twizy/src/Utilitaires/AnalyseVideo.java
@@ -1,11 +1,9 @@
-
-package video;
+package Utilitaires;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -19,22 +17,13 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-//import org.opencv.highgui.Highgui;
-//import org.opencv.highgui.VideoCapture;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.videoio.VideoCapture;
 
-import Utilitaires.Interface;
-import Utilitaires.MaBibliothequeTraitementImage;
-import Utilitaires.MaBibliothequeTraitementImageEtendue;
-
-import org.opencv.imgcodecs.Imgcodecs;
-//import org.opencv.videoio.VideoCapture;
 public class AnalyseVideo {
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -62,7 +51,7 @@ public class AnalyseVideo {
 					}
 				}
 				Trigger = false;
-				ImageIcon image = new ImageIcon(Function.Mat2bufferedImage(frame));
+				ImageIcon image = new ImageIcon(Mat2bufferedImage(frame));
 				vidpanel.setIcon(image);
 				Mat trans=MaBibliothequeTraitementImageEtendue.transformeBGRversHSV(frame);
 				Mat saturee=MaBibliothequeTraitementImage.seuillage(trans, 6, 170, 110);
@@ -72,8 +61,8 @@ public class AnalyseVideo {
 				List<MatOfPoint> ListeContours= MaBibliothequeTraitementImageEtendue.ExtractContours(saturee);
 				//Détection des objets
 				for (MatOfPoint contour: ListeContours  ){
-					objetrond=Function.DetectForm(frame,contour);
-					int indexmax=Function.identifiepanneau(objetrond);
+					objetrond=MaBibliothequeTraitementImageEtendue.DetectForm(frame,contour);
+					int indexmax=identifiepanneau(objetrond);
 					switch(indexmax){
 					case -1:;break;
 					case 0:System.out.println("Panneau 30 détécté");break;
@@ -94,6 +83,45 @@ public class AnalyseVideo {
 
 
 
+
+
+	public static BufferedImage Mat2bufferedImage(Mat image) {
+		MatOfByte bytemat = new MatOfByte();
+		Highgui.imencode(".jpg", image, bytemat);
+		byte[] bytes = bytemat.toArray();
+		InputStream in = new ByteArrayInputStream(bytes);
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return img;
+	}
+
+
+
+	public static int identifiepanneau(Mat objetrond){
+		double [] scores=new double [6];
+		int indexmax=-1;
+		if (objetrond!=null){
+			scores[0]=MaBibliothequeTraitementImageEtendue.Similitude(objetrond,"ref30.jpg");
+			scores[1]=MaBibliothequeTraitementImageEtendue.Similitude(objetrond,"ref50.jpg");
+			scores[2]=MaBibliothequeTraitementImageEtendue.Similitude(objetrond,"ref70.jpg");
+			scores[3]=MaBibliothequeTraitementImageEtendue.Similitude(objetrond,"ref90.jpg");
+			scores[4]=MaBibliothequeTraitementImageEtendue.Similitude(objetrond,"ref110.jpg");
+			scores[5]=MaBibliothequeTraitementImageEtendue.Similitude(objetrond,"refdouble.jpg");
+
+			double scoremax=scores[0];
+
+			for(int j=1;j<scores.length;j++){
+				if (scores[j]<scoremax){scoremax=scores[j];indexmax=j;}}	
+
+
+		}
+		return indexmax;
+	}
 
 
 }
--- Video/src/video/Function.java
+++ Video/src/video/Function.java
@@ -1,117 +1,87 @@
-package Utilitaires;
-import java.awt.Dimension;
+package video;
+
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Random;
-import java.util.Vector;
 
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
+import org.opencv.core.DMatch;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfDMatch;
-import org.opencv.core.MatOfInt4;
 import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.features2d.DMatch;
 import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
 import org.opencv.features2d.Features2d;
 import org.opencv.highgui.Highgui;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-public class MaBibliothequeTraitementImageEtendue {
-	//Contient toutes les méthodes necessaires à la transformation des images
+import Utilitaires.MaBibliothequeTraitementImageEtendue;
+
+public class Function {
 
 
-	//Methode qui permet de transformer une matrice intialement au  format BGR au format HSV
-	public static Mat transformeBGRversHSV(Mat matriceBGR){
-		Mat matriceHSV=new Mat(matriceBGR.height(),matriceBGR.cols(),matriceBGR.type());
-		Imgproc.cvtColor(matriceBGR,matriceHSV,Imgproc.COLOR_BGR2HSV);
-		return matriceHSV;
-
-	}
-
-	//Methode qui convertit une matrice avec 3 canaux en un vecteur de 3 matrices monocanal (un canal par couleur)
-	public static Vector<Mat> splitHSVChannels(Mat input) {
-		Vector<Mat> channels = new Vector<Mat>(); 
-		Core.split(input, channels);
-		return channels;
-	}
-
-	//Methode qui permet d'afficher une image sur un panel
-	public static void afficheImage(String title, Mat img){
-		MatOfByte matOfByte=new MatOfByte();
-		Highgui.imencode(".png",img,matOfByte);
-		byte[] byteArray=matOfByte.toArray();
-		BufferedImage bufImage=null;
-		try{
-			InputStream in=new ByteArrayInputStream(byteArray);
-			bufImage=ImageIO.read(in);
-			JFrame frame=new JFrame();
-			frame.setTitle(title);
-			frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
-			frame.pack();
-			frame.setVisible(true);
-
-		}
-		catch(Exception e){
+	public static BufferedImage Mat2bufferedImage(Mat image) {
+		MatOfByte bytemat = new MatOfByte();
+		Imgcodecs.imencode(".jpg", image, bytemat);
+		byte[] bytes = bytemat.toArray();
+		InputStream in = new ByteArrayInputStream(bytes);
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(in);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-
+		return img;
 	}
 
-	
+	public static int identifiepanneau(Mat objetrond){
+		double [] scores=new double [6];
+		int indexmax=-1;
+		if (objetrond!=null){
+			scores[0]=Similitude(objetrond,"ref30.jpg");
+			scores[1]=Similitude(objetrond,"ref50.jpg");
+			scores[2]=Similitude(objetrond,"ref70.jpg");
+			scores[3]= Similitude(objetrond,"ref90.jpg");
+			scores[4]=Similitude(objetrond,"ref110.jpg");
+			scores[5]=Similitude(objetrond,"refdouble.jpg");
 
-	//Methode qui permet d'extraire les contours d'une image donnee
-	public static List<MatOfPoint> ExtractContours(Mat input) {
-		// Detecter les contours des formes trouvées
-		int thresh = 100;
-		Mat canny_output = new Mat();
-		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		MatOfInt4 hierarchy = new MatOfInt4();
-		Imgproc.Canny( input, canny_output, thresh, thresh*2);
+			double scoremax=Double.POSITIVE_INFINITY;
+
+			for(int j=1;j<scores.length;j++){
+				if (scores[j]<scoremax){
+					scoremax=scores[j];
+					indexmax=j;}}	
 
 
-		/// Find extreme outer contours
-		Imgproc.findContours( canny_output, contours, hierarchy,Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-
-		Mat drawing = Mat.zeros( canny_output.size(), CvType.CV_8UC3 );
-		Random rand = new Random();
-		for( int i = 0; i< contours.size(); i++ )
-		{
-			Scalar color = new Scalar( rand.nextInt(255 - 0 + 1) , rand.nextInt(255 - 0 + 1),rand.nextInt(255 - 0 + 1) );
-			Imgproc.drawContours( drawing, contours, i, color, 1, 8, hierarchy, 0, new Point() );
 		}
-		//afficheImage("Contours",drawing);
-
-		return contours;
+		return indexmax;
 	}
 
-	//Methode qui permet de decouper et identifier les contours carrés, triangulaires ou rectangulaires. 
-	//Renvoie null si aucun contour rond n'a été trouvé.	
-	//Renvoie une matrice carrée englobant un contour rond si un contour rond a été trouvé
-	
-	
+
+	public static double angle(Point a, Point b, Point c) {
+		Point ab = new Point( b.x - a.x, b.y - a.y );
+		Point cb = new Point( b.x - c.x, b.y - c.y );
+		double dot = (ab.x * cb.x + ab.y * cb.y); // dot product
+		double cross = (ab.x * cb.y - ab.y * cb.x); // cross product
+		double alpha = Math.atan2(cross, dot);
+		return Math.floor(alpha * 180. / Math.PI + 0.5);
+	}
+
 	public  static Mat DetectForm(Mat img,MatOfPoint contour) {
 		MatOfPoint2f matOfPoint2f = new MatOfPoint2f();
 		MatOfPoint2f approxCurve = new MatOfPoint2f();
@@ -128,8 +98,8 @@ public class MaBibliothequeTraitementImageEtendue {
 		//on dit que c'est un cercle si l'aire occupé par le contour est à supérieure à  80% de l'aire occupée par un cercle parfait
 		if ((contourArea / (Math.PI*radius[0]*radius[0])) >=0.8) {
 			//System.out.println("Cercle");
-			Core.circle(img, center, (int)radius[0], new Scalar(255, 0, 0), 2);
-			Core.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
+			Imgproc.circle(img, center, (int)radius[0], new Scalar(255, 0, 0), 2);
+			Imgproc.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
 			Mat tmp = img.submat(rect.y,rect.y+rect.height,rect.x,rect.x+rect.width);
 			Mat sign = Mat.zeros(tmp.size(),tmp.type());
 			tmp.copyTo(sign);
@@ -139,11 +109,12 @@ public class MaBibliothequeTraitementImageEtendue {
 			Imgproc.approxPolyDP(matOfPoint2f, approxCurve, Imgproc.arcLength(matOfPoint2f, true) * 0.02, true);
 			long total = approxCurve.total();
 			if (total == 3 ) { // is triangle
+				//System.out.println("Triangle");
 				Point [] pt = approxCurve.toArray();
-				Core.line(img, pt[0], pt[1], new Scalar(255,0,0),2);
-				Core.line(img, pt[1], pt[2], new Scalar(255,0,0),2);
-				Core.line(img, pt[2], pt[0], new Scalar(255,0,0),2);
-				Core.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
+				Imgproc.line(img, pt[0], pt[1], new Scalar(255,0,0),2);
+				Imgproc.line(img, pt[1], pt[2], new Scalar(255,0,0),2);
+				Imgproc.line(img, pt[2], pt[0], new Scalar(255,0,0),2);
+				Imgproc.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
 				Mat tmp = img.submat(rect.y,rect.y+rect.height,rect.x,rect.x+rect.width);
 				Mat sign = Mat.zeros(tmp.size(),tmp.type());
 				tmp.copyTo(sign);
@@ -164,7 +135,7 @@ public class MaBibliothequeTraitementImageEtendue {
 					double ratio = Math.abs(1 - (double) rect.width / rect.height);
 					//drawText(rect.tl(), ratio <= 0.02 ? "SQU" : "RECT");
 					//System.out.println("Rectangle");
-					Core.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
+					Imgproc.rectangle(img, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar (0, 255, 0), 2);
 					Mat tmp = img.submat(rect.y,rect.y+rect.height,rect.x,rect.x+rect.width);
 					Mat sign = Mat.zeros(tmp.size(),tmp.type());
 					tmp.copyTo(sign);
@@ -179,26 +150,12 @@ public class MaBibliothequeTraitementImageEtendue {
 		return null;
 
 	}
-
-	
-
-	public static double angle(Point a, Point b, Point c) {
-		Point ab = new Point( b.x - a.x, b.y - a.y );
-		Point cb = new Point( b.x - c.x, b.y - c.y );
-		double dot = (ab.x * cb.x + ab.y * cb.y); // dot product
-		double cross = (ab.x * cb.y - ab.y * cb.x); // cross product
-		double alpha = Math.atan2(cross, dot);
-		return Math.floor(alpha * 180. / Math.PI + 0.5);
-	}
-
-	
-	//methode à completer
 public static double Similitude(Mat object,String signfile) {
 		
 
 		// Conversion du signe de reference en niveaux de gris et normalisation
 		//Mat panneauref = Imgcodecs.imread(signfile);
-		Mat panneauref =Highgui.imread(signfile,Highgui.CV_LOAD_IMAGE_COLOR);
+		Mat panneauref =Imgcodecs.imread(signfile);
 		float somme=0;
 		//int n=336;
 		
@@ -261,9 +218,5 @@ public static double Similitude(Mat object,String signfile) {
 	
 		return moyenne;}
 
-
-public static void  Test(){
-	System.out.println("Accesible ");
-}
-
+	
 }
