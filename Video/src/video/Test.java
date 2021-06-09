f95109b124f151e598bb0ba0610ff0ac92aa4fc3
Yuuuugo <69073794+Yuuuugo@users.noreply.github.com>
2021-06-09 23:04:52 +0200

    Ajout Tesseract

---
+++ Video/src/video/Test.java
@@ -0,0 +1,28 @@
+package video;
+
+
+import java.io.File;
+  
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+  
+public class Test {
+    public static void main(String[] args)
+    {
+        Tesseract tesseract = new Tesseract();
+        try {
+  
+            tesseract.setDatapath("D:/Tess4J/tessdata");
+  
+            // the path of your tess data folder
+            // inside the extracted file
+            String text = tesseract.doOCR(new File("ref110.jpg"));
+  
+            // path of your image file
+            System.out.print(text);
+        }
+        catch (TesseractException e) {
+            e.printStackTrace();
+        }
+    }
+}
